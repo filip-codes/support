9719119a476e7a8b507243f02d600353e9493e2e
netbounty <filip.petrovic@netbounty.de>
2022-07-15 23:18:05 +0200

    added Str.of() method

--- src/Str.ts
+++ src/Str.ts
@@ -1,3 +1,13 @@
+import { Stringable } from "./Stringable";
+
 export class Str {
-  //
+  /**
+   * Get a new stringable object from the given string.
+   *
+   * @param string string
+   * @returns Stringable
+   */
+  public static of(string: string): Stringable {
+    return new Stringable(string);
+  }
 }
