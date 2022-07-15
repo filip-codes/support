9ec540e6b4393614a00020c669961a57854d9683
netbounty <filip.petrovic@netbounty.de>
2022-07-15 23:11:37 +0200

    added constructor to Stringable class

--- src/Stringable.ts
+++ src/Stringable.ts
@@ -1,5 +1,15 @@
 export class Stringable {
-  public constructor() {
-    //
+  /**
+   * The underlying string value.
+   */
+  protected value: string;
+
+  /**
+   * Create a new instance of the class.
+   *
+   * @param string string
+   */
+  public constructor(string: string) {
+    this.value = string;
   }
 }
