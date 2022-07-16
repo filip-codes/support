68f0876982d51eb4969a70c30d139bf77860e711
netbounty <filip.petrovic@netbounty.de>
2022-07-16 21:45:00 +0200

    added Stringable.camel() method

--- src/Stringable.ts
+++ src/Stringable.ts
@@ -12,4 +12,18 @@ export class Stringable {
   public constructor(string: string) {
     this.value = string;
   }
+
+  /**
+   * Convert a value to camel case.
+   *
+   * @returns string
+   */
+  public camel(): string {
+    return this.value
+      .toLowerCase()
+      .replace(/\s/g, "")
+      .replace(/[^a-zA-Z0-9]+(.)/g, (from: string, char: string) =>
+        char.toUpperCase()
+      );
+  }
 }
