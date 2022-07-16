5732c686ecaf611a002e8bd20f6538f3d31f1322
netbounty <filip.petrovic@netbounty.de>
2022-07-16 21:46:12 +0200

    added tests for Str.camel() method

--- tests/str.test.ts
+++ tests/str.test.ts
@@ -4,4 +4,12 @@ describe("Str", () => {
   it("can be instantiated without throwing errors", () => {
     expect(() => new Str()).not.toThrow(TypeError);
   });
+
+  it("can convert a value to camel case", () => {
+    expect(Str.camel("Midonis_support_package")).toBe("midonisSupportPackage");
+
+    expect(Str.of("Midonis_support_package").camel()).toBe(
+      "midonisSupportPackage"
+    );
+  });
 });
